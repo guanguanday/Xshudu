7747697afee3df8913b8eb961e616be732098aa4
enci33 <1653541445@qq.com>
2018-01-19 18:35:00 +0800

    delete vux

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,16 +1,11 @@
 <template>
     <div>
-        <x-header>
-            <span>overwrite-left</span>
-            <x-icon slot="overwrite-left" type="navicon" size="35" style="fill:#fff;position:relative;top:-8px;left:-3px;"></x-icon>
-        </x-header>
         {{gettersMsg}}
     </div>
 </template>
 
 <script>
     import {mapGetters, mapActions} from 'vuex'
-    import {XHeader, Drawer}  from 'vux'
     export default {
         data() {
             return{
@@ -18,7 +13,7 @@
             }
         },
         components: {
-            XHeader,
+
         },
         computed: {
             ...mapGetters(['gettersMsg'])
